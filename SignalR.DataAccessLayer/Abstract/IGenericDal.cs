80e186e160df25182644555b01ba9663624a0f20
Haçik Ulu <hacikd2@gmail.com>
2023-11-27 13:20:52 +0300

    Add project files.

---
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    // T tipi sadece class olmalı.
+    public interface IGenericDal<T> where T : class
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        List<T> GetListAll();
+    }
+}
---
+++ SignalR.EntityLayer/Entities/Feature.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string FirstTitle { get; set; }
+        public string FirstDesc { get; set; }
+        public string SecondTitle { get; set; }
+        public string SecondDesc { get; set; }
+        public string LastTitle { get; set; }
+        public string LastDesc { get; set; }
+    }
+}
