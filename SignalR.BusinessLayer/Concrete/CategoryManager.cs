cbe043f42c673f1252c58a117a7dc835ac0dd7d4
Haçik Ulu <hacikd2@gmail.com>
2023-11-27 15:45:48 +0300

    Layers have been edited.

---
+++ SignalR.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        // EfCategoryDal Repositories ile contexte(dbye bağlanıyor.)
+        private readonly ICategoryDal _categoryService;
+
+        public CategoryManager(ICategoryDal categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public void TAdd(Category entity)
+        {
+            _categoryService.Add(entity);
+        }
+
+        public void TDelete(Category entity)
+        {
+            _categoryService.Delete(entity);
+        }
+
+        public Category TGetById(int id)
+        {
+            return _categoryService.GetById(id);
+        }
+
+        public List<Category> TGetListAll()
+        {
+            return _categoryService.GetListAll();
+        }
+
+        public void TUpdate(Category entity)
+        {
+            _categoryService.Update(entity);
+        }
+    }
+}
---
+++ SignalR.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        private readonly ITestimionalDal _testimonialDal;
+
+        public TestimonialManager(ITestimionalDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void TAdd(Testimional entity)
+        {
+            _testimonialDal.Add(entity);
+        }
+
+        public void TDelete(Testimional entity)
+        {
+            _testimonialDal.Delete(entity);
+        }
+
+        public Testimional TGetById(int id)
+        {
+           return _testimonialDal.GetById(id);
+        }
+
+        public List<Testimional> TGetListAll()
+        {
+            return _testimonialDal.GetListAll();
+        }
+
+        public void TUpdate(Testimional entity)
+        {
+            _testimonialDal.Update(entity);
+        }
+    }
+}
