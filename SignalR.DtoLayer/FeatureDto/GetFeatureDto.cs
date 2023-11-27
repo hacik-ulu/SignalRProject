293e74a3f016607328f0cae2cd7953aec5dd0cec
Haçik Ulu <hacikd2@gmail.com>
2023-11-27 16:21:17 +0300

    Dto's revised.

---
+++ SignalR.DtoLayer/FeatureDto/GetFeatureDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.FeatureDto
+{
+    public class GetFeatureDto
+    {
+        public int FeatureID { get; set; }
+        public string FirstTitle { get; set; }
+        public string FirstDesc { get; set; }
+        public string SecondTitle { get; set; }
+        public string SecondDesc { get; set; }
+        public string LastTitle { get; set; }
+        public string LastDesc { get; set; }
+    }
+}
---
+++ SignalR.DtoLayer/TestimionalDto/UpdateTestimionalDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.TestimionalDto
+{
+    public class UpdateTestimionalDto
+    {
+        public int TestimionalID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
