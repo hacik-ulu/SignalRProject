a61b71c3e9602546136b315d8238cc4f0296329b
Haçik Ulu <hacikd2@gmail.com>
2023-11-27 16:43:16 +0300

    Dto's and Entities have been mapped.

---
+++ SignalRApi/Mapping/TestimonialMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.TestimionalDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class TestimonialMapping:Profile
+    {
+        public TestimonialMapping()
+        {
+            CreateMap<Testimional, ResultTestimionalDto>().ReverseMap();
+            CreateMap<SocialMedia, CreateTestimionalDto>().ReverseMap();
+            CreateMap<SocialMedia, GetTestimionalDto>().ReverseMap();
+            CreateMap<SocialMedia, UpdateTestimionalDto>().ReverseMap();
+        }
+    }
+}
