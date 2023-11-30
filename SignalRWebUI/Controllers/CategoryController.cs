db4e03cac33c1287bf059b403668f5d05ef72b50
Haçik Ulu <hacikd2@gmail.com>
2023-11-30 17:05:54 +0300

    API Consumed - For Category -

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SignalRWebUI.Dtos.CategoryDtos;
 
 namespace SignalRWebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7038/api/Category");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // Listeleme ve Json dönüşümünde Deserialize.
+
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ SignalRWebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.CategoryDtos
+{
+    // UI için çalışan Dto --> Json'dan gelen dataları eşleştirmemiz lazım.
+    public class ResultCategoryDto
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
