3a8ea4323feb766225f85a3d16b7b9e285c508df
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-15 16:43:37 +0300

    Product and Category API's finished.

--- SignalR.DtoLayer/ProductDto/GetProductDto.cs
+++ SignalR.DtoLayer/ProductDto/GetProductDto.cs
@@ -9,7 +9,7 @@ namespace SignalR.DtoLayer.ProductDto
     public class GetProductDto
     {
         public int ProductID { get; set; }
-        public int ProductName { get; set; }
+        public string ProductName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
