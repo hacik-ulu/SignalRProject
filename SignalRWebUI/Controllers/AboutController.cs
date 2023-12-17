05be297b5bd4a1c178e8cf7bbbc33556d3170a1a
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-17 18:54:50 +0300

    AboutController's host number edited.

--- SignalRWebUI/Controllers/AboutController.cs
+++ SignalRWebUI/Controllers/AboutController.cs
@@ -15,7 +15,7 @@ namespace SignalRWebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7186/api/About");
+            var responseMessage = await client.GetAsync("https://localhost:7038/api/About");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -35,7 +35,7 @@ namespace SignalRWebUI.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(createAboutDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:7186/api/About", stringContent);
+            var responseMessage = await client.PostAsync("https://localhost:7038/api/About", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -45,7 +45,7 @@ namespace SignalRWebUI.Controllers
         public async Task<IActionResult> DeleteAbout(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync($"https://localhost:7186/api/About/{id}");
+            var responseMessage = await client.DeleteAsync($"https://localhost:7038/api/About/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -56,7 +56,7 @@ namespace SignalRWebUI.Controllers
         public async Task<IActionResult> UpdateAbout(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7186/api/About/{id}");
+            var responseMessage = await client.GetAsync($"https://localhost:7038/api/About/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -71,7 +71,7 @@ namespace SignalRWebUI.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(updateAboutDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("https://localhost:7186/api/About/", stringContent);
+            var responseMessage = await client.PutAsync("https://localhost:7038/api/About/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
