36d1f9d9e00fbe421c29d3eb624b7ab0c96855fa
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-18 16:46:38 +0300

    Interval method added.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,9 +1,23 @@
 ﻿// Hub Sınıfı bir sunucu/server görevi görecek (Dağıtım işlemi yapılacak.)
 using Microsoft.AspNetCore.SignalR;
+using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
-    public class SignalRHub:Hub
+    public class SignalRHub : Hub
     {
+        // sayfanın yenilenmeden tetikleyici ile verilerin getirilmesi.
+
+        SignalRContext _context;
+        public SignalRHub(SignalRContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SendCategoryCount()
+        {
+            var value = _context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+        }
     }
 }
