737953c098f4e6e042157d0770f86515314b285a
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-21 20:33:49 +0300

    MoneyCases added.

---
+++ SignalR.BusinessLayer/Concrete/MoneyCaseManager.cs
@@ -0,0 +1,52 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class MoneyCaseManager : IMoneyCaseService
+    {
+        private readonly IMoneyCaseDal _monyCaseDal;
+
+        public MoneyCaseManager(IMoneyCaseDal monyCaseDal)
+        {
+            _monyCaseDal = monyCaseDal;
+        }
+
+        public void TAdd(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MoneyCase TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MoneyCase> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TTotalMoneyCaseAmount()
+        {
+            return _monyCaseDal.TotalMoneyCaseAmount();
+        }
+
+        public void TUpdate(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
