564423f8c7f59d6a4f79d95697775c8c71f5e6c1
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-22 13:34:09 +0300

    SignalR statistics edited.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,5 +1,6 @@
 ﻿// Hub Sınıfı bir sunucu/server görevi görecek (Dağıtım işlemi yapılacak.)
 using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
@@ -8,16 +9,36 @@ namespace SignalRApi.Hubs
     {
         // sayfanın yenilenmeden tetikleyici ile verilerin getirilmesi.
 
-        SignalRContext _context;
-        public SignalRHub(SignalRContext context)
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
         {
-            _context = context;
+            _categoryService = categoryService;
+            _productService = productService;
         }
 
         public async Task SendCategoryCount()
         {
-            var value = _context.Categories.Count();
-            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+            var sendCategoryCount = _categoryService.TCategoryCount();
+            await Clients.All.SendAsync("ReceiveCategoryCount", sendCategoryCount);
         }
+
+        public async Task SendProductCount()
+        {
+            var sendProductCount = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", sendProductCount);
+        }
+
+        public async Task ActivePassiveCategoryCount()
+        {
+            var activeCategoryCount = _categoryService.TActiveCategoryCount();
+            var passiveCategoryCount = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", activeCategoryCount);
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", passiveCategoryCount);
+        }
+
+       
+
     }
 }
