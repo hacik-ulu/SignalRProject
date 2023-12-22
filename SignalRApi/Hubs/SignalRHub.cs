a8c93057af2c53f8488ecd2eafd1ed0718a02bcb
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-22 13:38:39 +0300

    SignalRHub class edited.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -18,27 +18,26 @@ namespace SignalRApi.Hubs
             _productService = productService;
         }
 
-        public async Task SendCategoryCount()
+        public async Task SendStatistic()
         {
             var sendCategoryCount = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", sendCategoryCount);
-        }
 
-        public async Task SendProductCount()
-        {
             var sendProductCount = _productService.TProductCount();
             await Clients.All.SendAsync("ReceiveProductCount", sendProductCount);
-        }
 
-        public async Task ActivePassiveCategoryCount()
-        {
             var activeCategoryCount = _categoryService.TActiveCategoryCount();
             var passiveCategoryCount = _categoryService.TPassiveCategoryCount();
+
             await Clients.All.SendAsync("ReceiveActiveCategoryCount", activeCategoryCount);
             await Clients.All.SendAsync("ReceivePassiveCategoryCount", passiveCategoryCount);
         }
 
+
+
        
+
+
 
     }
 }
