85735bd82475137c4db59f8b97c97ca80b609764
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-25 17:21:39 +0300

    Slider API finished.

--- SignalRApi/Controllers/SlidersController.cs
+++ SignalRApi/Controllers/SlidersController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.SliderDto;
 
 namespace SignalRApi.Controllers
 {
@@ -9,17 +10,18 @@ namespace SignalRApi.Controllers
     public class SlidersController : ControllerBase
     {
         private readonly ISliderService _sliderService;
-
-        public SlidersController(ISliderService sliderService)
+        private readonly IMapper _mapper;
+        public SlidersController(ISliderService sliderService, IMapper mapper)
         {
             _sliderService = sliderService;
+            _mapper = mapper;
         }
 
-        [HttpGet("SliderList")]
+        [HttpGet]
         public IActionResult SliderList()
         {
-            var sliderList = _sliderService.TGetListAll();
-            return Ok(sliderList);
+            var value = _mapper.Map<List<ResultSliderDto>>(_sliderService.TGetListAll());
+            return Ok(value);
         }
     }
 }
