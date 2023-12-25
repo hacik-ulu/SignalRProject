159587e1cce887746579ea25d1c06e84a7b16f06
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-25 16:24:31 +0300

    SliderAPI edited.

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -84,6 +84,10 @@ builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
 builder.Services.AddScoped<IMenuTableService, MenuTableManager>();
 builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
 
+// Slider API
+builder.Services.AddScoped<ISliderService, SliderManager>();
+builder.Services.AddScoped<ISliderDal, EfSliderDal>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
