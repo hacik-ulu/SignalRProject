89f9f026ffb7dd247a468ed1270313c8b6bff3d4
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-26 15:25:40 +0300

    Footer edited.

--- SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -14,7 +14,7 @@ namespace SignalRWebUI.ViewComponents.UILayoutComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7038/api/Testimonial");
+            var responseMessage = await client.GetAsync("https://localhost:7038/api/Contact");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
             return View(values);
