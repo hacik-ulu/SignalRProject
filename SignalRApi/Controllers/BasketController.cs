1a8c92ebdf2463516bb59fbcaaa9c0672a2bda1a
Haçik ULU <20091010058@ogr.halic.edu.tr>
2023-12-28 15:35:06 +0300

    List ProductName in Basket.

--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalRApi.Models;
 
 namespace SignalRApi.Controllers
 {
@@ -9,10 +12,12 @@ namespace SignalRApi.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketService _basketService;
+        private readonly SignalRContext _context;
 
-        public BasketController(IBasketService basketService)
+        public BasketController(IBasketService basketService, SignalRContext context)
         {
             _basketService = basketService;
+            _context = context;
         }
 
         [HttpGet("GetBasketByMenuTableID")]
@@ -20,6 +25,23 @@ namespace SignalRApi.Controllers
         {
             var basket = _basketService.TGetBasketByMenuTableNumber(id);
             return Ok(basket);
+        }
+
+        [HttpGet("BasketListByMenuTableWithProductName")]
+        public IActionResult BasketListByMenuTableWithProductName(int id)
+        {
+            var basketList = _context.Baskets.Include(x => x.Product).Where(y => y.MenuTableID == id)
+                 .Select(z => new ResultBasketListWithProducts
+                 {
+                     BasketID = z.BasketID,
+                     Count = z.Count,
+                     MenuTableID = z.MenuTableID,
+                     Price = z.Price,
+                     ProductID = z.ProductID,
+                     TotalPrice = z.TotalPrice,
+                     ProductName = z.Product.ProductName
+                 }).ToList();
+            return Ok(basketList);
         }
     }
 }
--- SignalRWebUI/Controllers/BasketsController.cs
+++ SignalRWebUI/Controllers/BasketsController.cs
@@ -14,7 +14,7 @@ namespace SignalRWebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7038/api/Basket/GetBasketByMenuTableID?id=3");
+            var responseMessage = await client.GetAsync("https://localhost:7038/api/Basket/BasketListByMenuTableWithProductName?id=3");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
