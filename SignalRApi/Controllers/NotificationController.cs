61a0421066f78c5a4ac63acd16fa79f376c8fc7c
Haçik ULU <20091010058@ogr.halic.edu.tr>
2024-01-30 21:27:52 +0300

    Bildirim temel işlemleri güncellendi.

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -9,30 +11,70 @@ namespace SignalRApi.Controllers
     public class NotificationController : ControllerBase
     {
         private readonly INotificationService _notificationService;
-
         public NotificationController(INotificationService notificationService)
         {
             _notificationService = notificationService;
         }
-
         [HttpGet]
-        public IActionResult GetNotificationList()
+        public IActionResult NotificationList()
         {
-            var getNotificationList = _notificationService.TGetListAll();
-            return Ok(getNotificationList);
+            return Ok(_notificationService.TGetListAll());
         }
-
-        // Status = False, Okunmamış Bildirimleri Getirecek.
-        [HttpGet("GetNotificationCountByStatusFalse")]
-        public IActionResult GetNotificationCountByStatusFalse()
+        [HttpGet("NotificationCountByStatusFalse")]
+        public IActionResult NotificationCountByStatusFalse()
         {
             return Ok(_notificationService.TNotificationCountByStatusFalse());
         }
-
-        [HttpGet("GetAllNotificationtByFalse")]
-        public IActionResult GetAllNotificationtByFalse()
+        [HttpGet("GetAllNotificationByFalse")]
+        public IActionResult GetAllNotificationByFalse()
         {
             return Ok(_notificationService.TGetAllNotificationtByFalse());
         }
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                Description = createNotificationDto.Description,
+                Icon = createNotificationDto.Icon,
+                Status = false,
+                Type = createNotificationDto.Type,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString())
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Ekleme işlemi başarıyla yapıldı");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            _notificationService.TDelete(value);
+            return Ok("Bildirim Silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Description = updateNotificationDto.Description,
+                Icon = updateNotificationDto.Icon,
+                Status = updateNotificationDto.Status,
+                Type = updateNotificationDto.Type,
+                Date = updateNotificationDto.Date
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Güncelleme işlemi başarıyla yapıldı");
+        }
+       
     }
 }
+
+// Video 137'deyiz.
