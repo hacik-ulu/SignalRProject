671668172b6869f9ef616649263171367f7a067e
Haçik ULU <20091010058@ogr.halic.edu.tr>
2024-02-10 18:53:16 +0300

    Mail Gönderme İşlemleri Tamamlandı.

--- SignalRWebUI/Controllers/LoginController.cs
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -32,6 +32,12 @@ namespace SignalRWebUI.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Login");
+        }
     }
 }
 
